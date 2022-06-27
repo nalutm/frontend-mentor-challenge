5cca0735aaf46773be6f8cc37009cc04cde7d3f3
nalutm <nayara.luiza21@gmail.com>
2022-06-27 20:52:24 -0300

    :recycle: refactor: Added DOMContentLoaded event

--- faq-accordion-card/assets/script.js
+++ faq-accordion-card/assets/script.js
@@ -1,21 +1,22 @@
-const $questions = document.querySelectorAll('[data-faq] dt');
-const $answers = document.querySelectorAll('.faq__answer');
-const $arrow = document.querySelectorAll('[data-arrow]');
+document.addEventListener('DOMContentLoaded', () => {
+  const $questions = document.querySelectorAll('[data-faq] dt');
+  const $answers = document.querySelectorAll('.faq__answer');
+  const $arrow = document.querySelectorAll('[data-arrow]');
 
-if ($questions && $answers && $arrow) {
   const active = 'active';
   const addClass = element => element.classList.add(active);
   const toggleClass = element => element.classList.toggle(active);
   const addEvent = (element, e, cb) => element.addEventListener(e, cb);
-  const arrayInteraction = element => 
-    element.forEach((question, index) => 
+  const arrayInteraction = element =>
+    element.forEach((question, index) =>
       addEvent(question, 'click', () => handleClick(index))
-  );
+    );
+  
   const handleClick = index => {
     toggleClass($questions[index]);
     toggleClass($arrow[index]);
     toggleClass($answers[index]);
-    $answers[index].classList.remove('inactive');
+      $answers[index].classList.remove('inactive');
   }
 
   $answers.forEach(answer => answer.classList.add('inactive'));
@@ -25,4 +26,5 @@ if ($questions && $answers && $arrow) {
   addClass($arrow[1]);
   arrayInteraction($questions);
   arrayInteraction($arrow);
-}
+      
+})
